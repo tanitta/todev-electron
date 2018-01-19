bfdeecfe4219df50c5ab933f3eb6c9dca62e7b09
tanitta <ttata.trit@gmail.com>
2018-01-19 12:54:23 +0900

    Improve visiblity of dependency-tree.

--- src/renderer/components/DepTree.vue
+++ src/renderer/components/DepTree.vue
@@ -50,27 +50,60 @@
         let canvasPos = document.getElementById('dep-tree-canvas').getBoundingClientRect()
 
         ctx.clearRect(0, 0, canvasPos.width, canvasPos.height)
+        ctx.fillStyle = '#AAAAAA'
+        ctx.strokeStyle = '#AAAAAA'
+        let offset = 10
+
+        this.tasks().forEach((task) => {
+          let taskRect = this.$refs['t' + task.id][0].getBoundingClientRect()
+          let center = {
+            x: offset,
+            y: taskRect.top - canvasPos.top + taskRect.height / 2
+          }
+          ctx.beginPath()
+          ctx.arc(center.x, center.y, 6, 0, Math.PI * 2, true)
+          ctx.fill()
+        })
 
         edges.forEach((edge) => {
           let refFrom = this.$refs['t' + edge.from][0].getBoundingClientRect()
           let refTo = this.$refs['t' + edge.to][0].getBoundingClientRect()
 
-          let height = refFrom.height
-
           let startPos = {
-            x: refFrom.left - canvasPos.left,
-            y: refFrom.top - canvasPos.top + height / 2
+            x: refFrom.left - canvasPos.left + offset,
+            y: refFrom.top - canvasPos.top + refFrom.height / 2
           }
 
           let finishPos = {
-            x: refTo.left - canvasPos.left,
-            y: refTo.top - canvasPos.top + height / 2
+            x: refTo.left - canvasPos.left + offset,
+            y: refTo.top - canvasPos.top + refTo.height / 2
           }
 
           let distance = Math.abs(startPos.y - finishPos.y)
+
+          let controlPoint = {
+            x: (startPos.x + finishPos.x) / 2 + distance * 0.3,
+            y: (startPos.y + finishPos.y) / 2
+          }
+
           ctx.beginPath()
           ctx.moveTo(startPos.x, startPos.y)
-          ctx.bezierCurveTo(startPos.x + distance / 2, startPos.y, finishPos.x + distance / 2, finishPos.y, finishPos.x, finishPos.y)
+          ctx.bezierCurveTo(
+            startPos.x + distance * 0.2,
+            startPos.y,
+            controlPoint.x,
+            controlPoint.y - distance / 2,
+            controlPoint.x,
+            controlPoint.y
+          )
+          ctx.bezierCurveTo(
+            controlPoint.x,
+            controlPoint.y + distance / 2,
+            finishPos.x + distance * 0.2,
+            finishPos.y,
+            finishPos.x,
+            finishPos.y
+          )
           ctx.stroke()
         })
       },
@@ -100,7 +133,6 @@
   }
   #dep-tree-canvas{
     position:absolute;
-    border:1px solid red;
   }
 
   #tree-container {
@@ -110,8 +142,12 @@
   }
 
   .task-name {
+    background-color: rgba(240, 240, 240, 0.5);
+    color: #AAAAAA;
     margin-left:100px;
-    padding: 6px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    padding:6px;
   }
   .task-container{
   }
